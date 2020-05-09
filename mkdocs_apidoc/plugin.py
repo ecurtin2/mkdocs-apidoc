966575a1dfe0384ea4c64b2070815e256f1c157e
ecurtin2 <ecurtin2@illinois.edu>
2020-05-09 17:24:41 -0500

    fix configuration of template bump to 1.6

--- mkdocs_apidoc/plugin.py
+++ mkdocs_apidoc/plugin.py
@@ -9,13 +9,18 @@ from mkdocs_apidoc import config
 class ApiDocPlugin(BasePlugin):
     config_scheme = (
         ("function_template", Type(str, default=config.function_template)),
-        ("bar", Type(int, default=0)),
-        ("baz", Type(bool, default=True)),
+        ("method_template", Type(int, default=config.method_template)),
+        ("signature_template", Type(bool, default=config.signature_template)),
+        ("module_template", Type(bool, default=config.module_template)),
+        ("class_template", Type(bool, default=config.class_template)),
     )
 
     def on_config(self, conf):
         config.function_template = self.config["function_template"]
-
+        config.method_template = self.config["method_template"]
+        config.signature_template = self.config["signature_template"]
+        config.module_template = self.config["module_template"]
+        config.class_template = self.config["class_template"]
         return conf
 
     def on_page_markdown(self, markdown: str, config, **kwargs):
